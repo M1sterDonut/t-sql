1803de40f5327a830eb46e9618a10f5f4f188321
M1sterDonut <lhg@tuta.io>
2023-09-23 21:23:32 +0100

    Update PRINT statement and formatting

--- T-SQL_06_Cursors.sql
+++ T-SQL_06_Cursors.sql
@@ -7,18 +7,18 @@
 -- @@FETCH_STATUS 0, data available; -1 no data left
 
 -- Cursor Stages
----- Declare Cursor         DECLARE cursor_name CURSOR FOR + SELECT statement      not DB object, declared like variable
----- Open                   OPEN cursor_name                                following the SELECT statement will be executed and result set generated 
----- Fetch                  FETCH NEXT FROM cursor_name INTO @var1, @var2   start pointing one-by-one
----- Close                  CLOSE cursor_name                               Cursor no longer available for fetching
----- Deallocate             DEALLOCATE cursor_name                          deallocate resources from cursor
+---- Declare Cursor         DECLARE cursor_name CURSOR FOR + SELECT statement       not DB object, declared like variable
+---- Open                   OPEN cursor_name                                        following the SELECT statement will be executed and result set generated 
+---- Fetch                  FETCH NEXT FROM cursor_name INTO @var1, @var2           start pointing one-by-one
+---- Close                  CLOSE cursor_name                                       Cursor no longer available for fetching
+---- Deallocate             DEALLOCATE cursor_name                                  deallocate resources occupied by cursor
+
 
 BEGIN
     DECLARE @bike_model VARCHAR(50);
     DECLARE @list_price NUMERIC(6,2);
     DECLARE @model_year NUMERIC(4);
     
-
     DECLARE bikecur CURSOR FOR
     SELECT 
         [product_name], 
@@ -27,9 +27,14 @@ BEGIN
     FROM
         [production].[products]
     WHERE
-        [list_price] = 999.99;
-    
+        [list_price] = 999.99; 
     OPEN bikecur;
+
+    BEGIN
+        PRINT 'We offer the following bikes for the incredible price of $999.99:'
+        PRINT ''
+    END
+
     FETCH NEXT FROM bikecur INTO @bike_model, @list_price, @model_year;
     WHILE @@FETCH_STATUS = 0
     BEGIN
